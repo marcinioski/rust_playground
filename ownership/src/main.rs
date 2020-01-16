8af106cef47d5ffa509b076b64ceda8051db4991
marcin.hujdus <marcin.hujdus@aptiv.com>
2020-01-16 08:44:14 +0100

    [LEARN] ownership and borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -27,6 +27,13 @@ fn main() {
         println!("{}", takes_ownership_and_return_copy(s4));
 
         get_reference(&s3);
+        
+        // does not compile because s4 is not mutable and previously was moved
+        // takes_ownership_mutate(&s4);
+        
+        let mut s4 = s3.clone();
+        // now it works
+        takes_ownership_mutate(&mut s4);
     }
     // s2 was moved in scope above!
     //println!("ex: {}", s2);
@@ -38,6 +45,11 @@ fn takes_ownership_and_return_copy(string: String) -> String {
 
 fn takes_ownership_and_return_copy_and_size(string: String) -> (usize, String) {
     (string.len(), string)
+}
+
+fn takes_ownership_mutate(string: &mut String) -> usize {
+    string.push_str("appended");
+    string.len()
 }
 
 fn get_reference(string: &String) -> usize {
